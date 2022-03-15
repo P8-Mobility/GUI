53d5cbab014e7af8fd5812323127b2e3fb605447
Niels Vistisen <nielswps@gmail.com>
2022-03-15 14:52:48 +0100

    Basic design done (sound not yet implemented)

--- App.js
+++ App.js
@@ -13,10 +13,12 @@ import {
     SafeAreaView,
     StatusBar,
     StyleSheet,
-    Text,
+    Text, TouchableOpacity,
     useColorScheme,
     View,
 } from 'react-native';
+
+const Sound = require('react-native-sound')
 
 const Header = ({title}): Node => {
     const isDarkMode = useColorScheme() === 'dark';
@@ -26,7 +28,7 @@ const Header = ({title}): Node => {
                 style={[
                     styles.title,
                     {
-                        color: isDarkMode ? "#fff" : "#000",
+                        color: isDarkMode ? "#fff" : "#fff",
                     },
                 ]}>
                 {title}
@@ -35,42 +37,47 @@ const Header = ({title}): Node => {
     );
 };
 
+const colorScheme = {"primary": "dodgerblue", "secondary": "lavender"}
+
 const App: () => Node = () => {
     const isDarkMode = useColorScheme() === 'dark';
+    const sound = new Sound('paere.wav', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+            console.log("Unable to load native pronunciation sample:", error.message);
+        }
+    })
     let [feedback: string, setFeedback] = useState("Press start to start recording");
     let [result: boolean, setResult] = useState(false);
+
+    function playPronunciationSample(): void {
+        if (sound) {
+            sound.play(success => {
+                if (!success) {
+                    console.log("Unable to play native pronunciation sample")
+                }
+                sound.release()
+            });
+        } else {
+            console.log("Unable to play native pronunciation sample")
+        }
+    }
 
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
             <Header title={"App"}/>
-            <View
-                style={{
-                    color: isDarkMode ? "#fff" : "#000",
-                    flex: 1,
-                }}>
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: "center",
-                    alignContent: "center",
-                    padding: 10,
-                }}>
+            <View style={styles.container}>
+                <View style={styles.row}>
                     <Text style={styles.word}>"Pære"</Text>
-                    <View style={{
-                        justifyContent: "center",
-                        alignContent: "center",
-                    }}>
-                        <Button title={"Play"} style={{
-                            alignSelf: "center",
-                            textAlign: "center",
-                        }}/>
-                    </View>
+                    <TouchableOpacity onPress={() => playPronunciationSample()} style={styles.buttonSmall}>
+                        <Text style={styles.buttonText}>Listen</Text>
+                    </TouchableOpacity>
                 </View>
-                <Text style={styles.text}>{feedback}</Text>
+                <Text style={styles.feedback}>{feedback}</Text>
             </View>
-            <View style={styles.buttonView}>
-                <Button title={"Start"} onPress={() => {setFeedback("No")}}/>
-            </View>
+            <TouchableOpacity onPress={() => setFeedback("No")} style={styles.button}>
+                <Text style={styles.buttonText}>Start</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 };
@@ -79,30 +86,57 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: "center",
+        alignContent: "center",
+        padding: 10,
+    },
     header: {
         paddingVertical: 10,
         alignSelf: "stretch",
-        backgroundColor: "#8a2be2"
+        backgroundColor: colorScheme.primary,
     },
     title: {
-        fontSize: 24,
+        fontSize: 32,
         fontWeight: '600',
         alignSelf: "center",
     },
-    text: {
+    feedback: {
         padding: 20,
         fontSize: 24,
-        alignSelf: "center"
-    },
-    buttonView: {
-        width: "100%",
         alignSelf: "center",
-        padding: 50,
+        flex: 1,
+    },
+    button: {
+        alignSelf: "center",
+        backgroundColor: colorScheme.primary,
+        borderRadius: 10,
+        paddingVertical: 20,
+        paddingHorizontal: 80,
+        margin: 20,
+        elevation: 5,
+    },
+    buttonText: {
+        fontSize: 24,
+        alignSelf: "center",
+        color: "white",
+    },
+    buttonSmall: {
+        alignSelf: "center",
+        backgroundColor: colorScheme.primary,
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        elevation: 5,
     },
     word: {
         fontSize: 48,
         alignSelf: "center",
         padding: 10,
+        marginEnd: 10,
+        textDecorationLine: "underline",
     }
 });
 
